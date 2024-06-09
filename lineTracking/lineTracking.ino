ef40cb0640a4f212bf759077424c5323f8b13b36
Bendik Aas <bendikaas00@gmail.com>
2024-06-09 19:25:57 +0200

    la til obsticle avoidance og greier for armene

--- lineTracking/lineTracking.ino
+++ lineTracking/lineTracking.ino
@@ -2,37 +2,103 @@
 
 #define D10 10 // Left Wheel
 #define D11 11 // Right Wheel
+#define D12 12 // Left arm
+#define D13 13 // Right arm
 #define A0_PIN A0 // IR sensor analog input
 
 // Global Variables
-Servo _servoD10;
-Servo _servoD11;
+Servo _servoD10; // Left wheel
+Servo _servoD11; // Right wheel
+Servo _servoD12; // Left arm
+Servo _servoD13; // Right arm
 
-int velocity = 15;
-int gain = 8;
+int velocity = 8;
+int gain = 10;
 int leftSpeed = 90;
 int rightSpeed = 90;
 
-// Line following functions
+const int trigPin = 3; // sonar
+const int echoPin = 4; // sonar
+const int distanceThreshold = 20; // Distance threshold in cm
+
+// Functions
 void move_base_D10_D11(int speed, int turn);
 int follow(int light, int gain);
+void armsIn();
+void armsOut();
+void armsStraight();
+void driveStraight();
+void driveBackwards();
+int getDistance();
+bool obsticleDetected();
 
 void setup() {
   Serial.begin(9600);
+
   _servoD10.attach(D10);
   _servoD11.attach(D11);
+  _servoD12.attach(D12);
+  _servoD13.attach(D13);
   pinMode(A0_PIN, INPUT);
+  pinMode(trigPin, OUTPUT); //SONAR
+  pinMode(echoPin, INPUT); //SONAR
 
   _servoD10.write(leftSpeed); // Stop position
   _servoD11.write(rightSpeed); // Stop position
+  _servoD12.write(90); // arm straight
+  _servoD13.write(90); // arm straight
 
   delay(1000); // Give some time for setup
 }
 
 void loop(){
   lineTracking();
-  
   delay(100); // Small delay for smoother operation
+}
+
+bool obsticleDetected(){
+  int distance = getDistance();
+  return (distance < distanceThreshold);
+}
+
+int getDistance(){
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  long duration = pulseIn(echoPin, HIGH);
+  int distance = (duration * 0.0343) / 2; // [cm]
+  Serial.print("Distance: ");
+  Serial.println(distance);
+
+  return distance;
+}
+
+void armsIn(){
+  _servoD12.write(50);
+  _servoD13.write(130);
+}
+
+void armsOut(){
+  _servoD12.write(140);
+  _servoD13.write(40);
+}
+
+void armsStraight(){
+  _servoD12.write(90);
+  _servoD13.write(90);
+}
+
+void driveStraight(){
+  _servoD10.write(100); // left wheel
+  _servoD11.write(75); // right wheel
+}
+
+void driveBackwards(){
+  _servoD10.write(80); // left wheel
+  _servoD11.write(93); // right wheel
 }
 
 void lineTracking(){
@@ -46,18 +112,14 @@ int follow(int light, int gain) {
   Serial.println(light);
 
   int black = 100; // Above this is black
-  int white = 35;  // Below this is white
-  // int mean = (black + white) / 2;
-  // int deadZone = 30;
+  int white = 50;  // Below this is white
 
-  if (light > black) {// Black detected -> turn right
-    return -gain; // turn  = -8
-  }
-  
-  else if (light < white) {// White detected -> turn left
-    return gain;
-  }
-  
+  if (light > black) { // Black detected -> turn right
+    return -gain; // turn  = -20
+  } 
+  else if (light < white) { // White detected -> turn left
+    return gain; // turn  = 20
+  } 
   else { // In the middle section -> Drive straight ahead
     return 0; // No turn
   }
@@ -65,15 +127,14 @@ int follow(int light, int gain) {
 
 void move_base_D10_D11(int speed, int turn){
   if (turn == 0){
-    leftSpeed = 95;
-    rightSpeed = 80;
-  }
-  else{
+    driveStraight();
+  } 
+  else {
     leftSpeed = 90 - turn;
     rightSpeed = 90 - turn;
+    _servoD10.write(leftSpeed);
+    _servoD11.write(rightSpeed);
   }
-  _servoD10.write(leftSpeed);
-  _servoD11.write(rightSpeed);
 
   Serial.print("Left Wheel: ");
   Serial.print(leftSpeed);
